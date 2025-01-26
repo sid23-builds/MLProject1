1b26e3b950ff7d49d3973fde7188b3b120db393f
sid23-builds <jain02.sid@gmail.com>
2025-01-26 11:27:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
