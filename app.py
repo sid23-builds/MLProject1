986f2a9e59896bae6c36c96c0a2faa689b3a16a9
sid23-builds <jain02.sid@gmail.com>
2025-01-26 14:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 from src.exception import CustomException
